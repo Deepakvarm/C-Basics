5841e6053db381aef02714f44e5ff45ee8f21be1
Deepakvarm <92075023+Deepakvarm@users.noreply.github.com>
2021-10-21 10:26:31 +0530

    Add files via upload

--- c+name.c
+++ c+name.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 void main()
-{
-  printf("rollnumber: 20mh5a0305\nname: deepak varma\nbranch: mechanical\ncollege: adtiya collage of engineering");
-  
+{ printf("my name is \nd\ne\ne\np\na\nk\nv\na\nr\nm\na");
 }
+
 
