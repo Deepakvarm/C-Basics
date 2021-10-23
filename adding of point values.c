6231953471c2c00a57aa38ba5be0ca067706121f
Deepakvarm <92075023+Deepakvarm@users.noreply.github.com>
2021-10-23 10:56:35 +0530

    Add files via upload

---
+++ adding of point values.c
@@ -0,0 +1,21 @@
+//adding two point values//
+#include<stdio.h>
+void main()
+{
+	//variable decleration//
+	
+     float number1,number2,sum;
+     
+     //initialization//
+     
+     number1=30.652;
+     number2=60.892;
+     
+     //calculation//
+     
+     sum=number1+number2;
+     
+     //output//
+     printf("%f",sum);
+     
+}
---
+++ new prog on data type.c
@@ -0,0 +1,20 @@
+/*data type variable program*/
+#include<stdio.h>
+void main()
+{ 
+  //variable decleration
+  int age;
+  float cgpa;
+  char name;
+    
+  //initilalization
+  age=20;
+  cgpa=7.5;
+  name='d';
+    
+    //output
+    printf("age of the student is %d\n",age);
+    printf("cgpa of the student is %f\n",cgpa);
+    printf("student name is %c",name);
+    
+}
