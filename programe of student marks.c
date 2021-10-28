93df4d4ec599ec9e5f9c3ffb3798bde5c348d9b8
Deepakvarm <92075023+Deepakvarm@users.noreply.github.com>
2021-10-28 10:53:28 +0530

    Add files via upload

---
+++ programe of student marks.c
@@ -0,0 +1,32 @@
+ #include<stdio.h>
+ void main()
+ {
+ 	
+ 	   //variable decelaration
+ 	   
+ 	   int phy,mat,eng,che,tot;
+ 	   
+ 	   //initilization
+ 	   
+ 	   phy=70;
+ 	   mat=40;
+ 	   che=60;
+ 	   eng=90;
+ 	   
+ 	 //caculation
+ 	 
+ 	 tot=phy+che+mat+eng;
+ 	 
+ 	 //output
+ 	 
+ 	 
+ 	 printf("marks in physics:%d,\n",phy);
+ 	 
+ 	 printf("marks in maths:%d,\n",mat);
+ 	 
+ 	 printf("marks in chemestry:%d,\n",che);
+ 	 
+ 	 printf("marks in english:%d,\n",eng);
+ 	 
+ 	 printf("tot marks is:%d",tot);
+ }
