711149180378855261e09fc00d01c938f3e4954c
Deepakvarm <92075023+Deepakvarm@users.noreply.github.com>
2021-10-30 10:17:30 +0530

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,27 @@
+#include<stdio.h> 
+void main()
+{
+	//variable decelarationinclude
+  
+  int k,j,a,s,d;
+  
+  //initilization
+  
+  k=20;
+  j=50;
+  a=10;
+  s=80;
+  
+  //calculation
+  
+  d=k+j+a+s;
+  
+  //output
+  
+   printf("marks of k =%d\n",k);
+   printf("marks of j =%d\n",j);
+   printf("marks of a =%d\n",a);
+   printf("marks of s =%d\n",s);
+   printf("total marks= %d",d);
+}
+  
