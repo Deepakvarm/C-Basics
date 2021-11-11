714f28ace397e29002a847b26a9418504849964f
Deepakvarm <92075023+Deepakvarm@users.noreply.github.com>
2021-11-11 10:50:50 +0530

    Add files via upload

---
+++ area$perimeter of square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int side,area,perimeter;
+	printf("enter side length");
+	scanf("%d",&area);
+	printf("area of square is %d\n",area*area);
+	printf("perimeter of square is %d",4*area);
+}
